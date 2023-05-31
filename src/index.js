853fdebee247a6c30f4ea6120abaf2a6328f1234
dishant42 <dishantgarg57@gmail.com>
2023-05-31 08:44:00 +0530

    sequelize completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express=require("express");
+const bodyparser=require("body-parser");
+
 const {PORT}=require("./config/ServerConfig");
 
 const setupAndStartServer= async()=>{
 
     const app=express();
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         console.log(`server started at ${PORT}`);
         console.log(process.env)    })
