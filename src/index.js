91b086a5336f8ba1bd4e6f12f2dbea52125c722d
dishant42 <dishantgarg57@gmail.com>
2023-06-04 15:15:09 +0530

    rewritten respository

--- src/index.js
+++ src/index.js
@@ -23,3 +23,6 @@ setupAndStartServer();
 // // Create a new user
 // const jane = await User.create({ firstName: "Jane", lastName: "Doe" });
 // console.log("Jane's auto-generated ID:", jane.id);
+
+// The User.create method is being called to create a new user object. The method is assumed to be asynchronous and returns a promise.
+// The object being passed as an argument { firstName: "Jane", lastName: "Doe" } contains the properties for the new user, such as the first name and last name.
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,34 +1,63 @@
 // repository is for interatciong with model;
 
-const {City}=require("../models/index")
+const { City } = require("../models/index")
 
-class CityRepository{
-    async createCity({name}){
-        try{
-            const city=await City.create({name});
-            return city;
-        }catch(error){
-            throw{error};
+class CityRepository {
+    async createcity({ name }) {
+        try {
+            const city = await City.create({
+                name: name
+            });
+        } catch (error) {
+            console.log("some error has occured in repository layer");
+            throw (error);
         }
 
-    }
+    };   //name:name is same as name
 
-    async deleteCity(cityId){
-        try{
+
+    async destroycity({ cityID }) {
+        try {
             await City.destroy({
-                where:{
-                    id:cityId
+                where: {
+                    id: cityID
                 }
             });
-        }catch(error){
-            throw{error};
+
+        } catch (error) {
+            console.log("some error has occured in repository layer");
+            throw (error);
         }
-    }
+
+    };
+
+    async updatecity(cityID, data) {
+        try {
+            const city = await City.update({      //The City.update method is called, indicating that an 
+                                                //update operation should be performed on city model
+                where: {                         //If the update operation is successful, the city variable is   assigned the result of the update operation, which could be the number of affected rows or or any other relevant information.
+                    id: cityID
+                }
+
+            })
+            return city;
+        } catch (error) {
+            console.log("some error has occured in repository layer");
+            throw (error);
+        }
+    };
 
 
-
-
+    async getcity(cityID) {
+        try {
+            const city =await City.findByPK(cityID);
+            return city;
+        } catch (error) {
+            console.log("some error has occured in repository layer");
+            throw (error);
+        }
+     };
 
 }
 
-module.exports=CityRepository;
+module.exports = CityRepository;
