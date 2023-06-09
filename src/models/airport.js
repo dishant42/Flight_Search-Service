0d79e233e8b1fc14868225a70521561e5edc87cb
dishant42 <dishantgarg57@gmail.com>
2023-06-09 12:38:04 +0530

    defined associations

--- src/models/airport.js
+++ src/models/airport.js
@@ -3,26 +3,33 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      
       // define association here
-      
-      this.hasMany(models.Airport,{
-       foreignKey:'cityID'
+      this.belongsTo(models.City,{
+        foreignKey:"cityID",
+        onDelete:"CASCADE"
       })
     }
   }
-  City.init({
-    name: {type:DataTypes.STRING,allowNull:false,unique:true}
+  Airport.init({
+    name: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    address: DataTypes.STRING,
+    cityID: {
+      type:DataTypes.INTEGER,
+      allowNull:false
+    }
   }, {
     sequelize,
-    modelName: 'City',
+    modelName: 'Airport',
   });
-  return City;
+  return Airport;
 };
